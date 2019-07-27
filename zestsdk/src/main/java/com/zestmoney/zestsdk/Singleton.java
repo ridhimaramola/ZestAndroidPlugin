7b8c987fd11c7f9b5ec62e29c999ff39d0a20bf6
Ridhima <ridhima.ramola@zestmoney.in>
2019-07-27 10:18:40 +0530

    Test Commit

---
+++ zestsdk/src/main/java/com/zestmoney/zestsdk/Singleton.java
@@ -0,0 +1,11 @@
+package com.zestmoney.zestsdk;
+
+import android.content.Context;
+
+public class Singleton {
+
+    public static String getTextFromPlugin (int number)
+    {
+        return "Number is "+number;
+    }
+}
